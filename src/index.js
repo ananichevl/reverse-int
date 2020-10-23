28de5a79920d2b7dadd75aefc6ba61b1706be2ac
Ананичев Леонид Дмитриевич <ananichev.l.dm@sberbank.ru>
2020-10-23 18:50:39 +0300

    feat: addReverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let number = n.toString();
+
+    let sign = number[0] === '-' ? '-' : '';
+
+    if(sign) {
+        number = number.slice(1);
+    }
+
+    let result = '';
+
+    for (let i = number.length - 1; i >= 0; i--) {
+        result += number[i];
+    }
+
+    return result;
+};
